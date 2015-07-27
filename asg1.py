401f09fd40c8b7979dea1dd5b90ebec53cb02cf5
Brett <bmgeorg@g.clemson.edu>
2015-07-27 14:46:02 -0400

    Part A

--- asg1.py
+++ asg1.py
@@ -1,4 +1,64 @@
-rules = {}
+import sys
+import random
+
+# Should be constructed with CfgBuilder
+class Cfg:
+    # public interface
+    def sentence(self):
+        return self.reduce('ROOT')
+
+    # private interface
+    class Rule:
+        weight = 0.0
+        rhs = []
+        def __init__(self, weight, rhs):
+            self.weight = weight
+            self.rhs = rhs
+
+    # maps lhs -> [Rule]
+    rules = {}
+    
+    def reduce(self, lhs):
+        if not self.is_nonterminal(lhs):
+            return lhs + ' '
+
+        lhs_rules = self.rules.get(lhs)
+        # pick random rule
+        i = random.randint(0, len(lhs_rules)-1)
+
+        rhs = lhs_rules[i].rhs
+
+        result = ''
+        for x in rhs:
+            result += self.reduce(x)
+
+        return result     
+
+    def print_out(self):
+        for lhs, x in sorted(self.rules.iteritems()):
+            for rhs in x:
+                print lhs + ' -> ' + str(rhs[0]) + ' '  +  ' '.join(rhs[1])
+
+    def is_nonterminal(self, lhs):
+        return self.rules.get(lhs) is not None
+
+class CfgBuilder:
+    # maps lhs -> [Rule]
+    rules = {}
+
+    def add_rule(self, lhs, rhs, weight):
+        lhs_rules = self.rules.get(lhs, [])
+        lhs_rules.append(Cfg.Rule(weight, rhs))
+        self.rules[lhs] = lhs_rules
+
+    def build(self):
+        cfg = Cfg()
+        cfg.rules = self.rules
+        if not cfg.is_nonterminal('ROOT'):
+            raise ValueError('No ROOT nonterminal')
+
+        self.rules = {}
+        return cfg
 
 def is_number(x):
     try:
@@ -7,17 +67,8 @@ def is_number(x):
     except:
         return False
 
-def make_rule(tokens):
-    if len(tokens) < 3 or not is_number(tokens[0]) or float(tokens[0]) < 0:
-        raise ValueError()
-    num = float(tokens[0])
-    rhs = tokens[1]
-    lhs = tokens[2:]
-    x = rules.get(rhs, []);
-    x.append((num, lhs))
-    rules[rhs] = x
-
-def read_file(file):
+def read_grammar(file):
+    builder = CfgBuilder()
     f = open(file, 'r')
     for i, line in enumerate(f):
         # ignore comments
@@ -30,13 +81,17 @@ def read_file(file):
             continue
 
         # skip ill-formatted lines
-        try:
-            make_rule(tokens)
-        except ValueError:
+        if len(tokens) < 3 or not is_number(tokens[0]) or float(tokens[0]) <= 0:
             print 'Invalid format on line ' + str(i) + '. Skipping line.'
             continue
+        
+        builder.add_rule(tokens[1], tokens[2:], tokens[0])
 
-read_file('grammar.txt')
-for x, y in sorted(rules.iteritems()):
-    for z in y:
-        print x + ' -> ' + str(z[0]) + ' '  +  ' '.join(z[1])
+    return builder.build()
+
+cfg = read_grammar(sys.argv[1])
+n = 1
+if len(sys.argv) >= 3:
+    n = int(sys.argv[2])
+for i in range(0, n):
+    print cfg.sentence()
